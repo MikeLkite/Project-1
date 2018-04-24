0de9afe22c6de3a8e1c8fa3a09667edcea25160f
MikeLkite <30781691+MikeLkite@users.noreply.github.com>
2018-04-24 14:33:43 +0100

    V1.9
    V1.9

--- JS/game.js
+++ JS/game.js
@@ -1,3 +1,27 @@
 window.onload = function() {
     document.getElementById("rngout").innerHTML = Math.floor(Math.random() * 11);;
 }
+
+document.getElementById('submit').onclick = compare;
+
+function compare() {
+
+  var a =  document.getElementById("rngout").innerHTML
+  a = parseFloat(a);
+
+  var b = document.getElementById("usernum").value;
+  b = parseFloat(b);
+
+  var comparison;
+
+  if(a > b) {
+    comparison = 'HIGHER';
+  } else if(a < b) {
+    comparison = 'LOWER';
+  } else {
+    comparison = 'CORRECT';
+  }
+
+   document.getElementById('comparison').innerHTML = comparison;
+
+}
